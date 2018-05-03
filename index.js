ff6a9dbae2f62bf19e4b3663a8e1f155a1847e36
TakingTheL <yliu6@stuy.edu>
2018-05-02 21:06:54 -0400

    overwrote old file

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var popTotal = function(biggyData){
 	.reduce(function(a,b){return (a+b)});
 };
 
-<<<<<<< HEAD
 var workingAge = function(biggyData){ //working age is 15 to 64
     return biggyData
 	.filter(function(year){return year['age'] >= 15 && year['age'] <= 64})
@@ -24,23 +23,6 @@ var percentGender = function(biggyData){
     males = 100 * males / total
     females = 100 * females / total
     return [males, females]
-=======
-var popTotal = function(biggyData)
-{
-	var totalsList = biggyData.map(function(n){
-		return n["total"];
-	})
-	//console.log(totalsList);
-	return totalsList.reduce(function(a,b){
-		/*
-		console.log("A:");
-		console.log(a);
-		console.log("B:");
-		console.log(b);
-		*/
-		return(a+b);
-	});
->>>>>>> 4d76eeb0afe4c4748395b99df5c9405cfe30109e
 }
 
 var retirementAge = function(biggyData){
